18a8ad3cae94a65325d35837096fc30488e849d6
ukieiri <ukieiri@gmail.com>
2014-11-27 15:02:14 +0100

    fdsf
    fdsfs

---
+++ GurshchenkovaValette/GurshchenkovaValette/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace GurshchenkovaValette
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbMainPicture = new System.Windows.Forms.PictureBox();
+            this.pbMiamiFilter = new System.Windows.Forms.PictureBox();
+            this.btMiamiFilter = new System.Windows.Forms.Button();
+            this.pbNightFilter = new System.Windows.Forms.PictureBox();
+            this.btNightFilter = new System.Windows.Forms.Button();
+            this.pbHellFilter = new System.Windows.Forms.PictureBox();
+            this.btHellFilter = new System.Windows.Forms.Button();
+            this.btLoad = new System.Windows.Forms.Button();
+            this.pbZenFilter = new System.Windows.Forms.PictureBox();
+            this.pbBlackAndWhite = new System.Windows.Forms.PictureBox();
+            this.pbSwapFilter = new System.Windows.Forms.PictureBox();
+            this.btZenFilter = new System.Windows.Forms.Button();
+            this.btBlackAndWhite = new System.Windows.Forms.Button();
+            this.btSwapFilter = new System.Windows.Forms.Button();
+            this.lbImages = new System.Windows.Forms.ListBox();
+            this.btSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMainPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMiamiFilter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNightFilter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHellFilter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbZenFilter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlackAndWhite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSwapFilter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbMainPicture
+            // 
+            this.pbMainPicture.Location = new System.Drawing.Point(13, 13);
+            this.pbMainPicture.Name = "pbMainPicture";
+            this.pbMainPicture.Size = new System.Drawing.Size(622, 394);
+            this.pbMainPicture.TabIndex = 0;
+            this.pbMainPicture.TabStop = false;
+            // 
+            // pbMiamiFilter
+            // 
+            this.pbMiamiFilter.Location = new System.Drawing.Point(13, 465);
+            this.pbMiamiFilter.Name = "pbMiamiFilter";
+            this.pbMiamiFilter.Size = new System.Drawing.Size(120, 82);
+            this.pbMiamiFilter.TabIndex = 1;
+            this.pbMiamiFilter.TabStop = false;
+            this.pbMiamiFilter.Click += new System.EventHandler(this.pbMiamiFilter_Click);
+            // 
+            // btMiamiFilter
+            // 
+            this.btMiamiFilter.Location = new System.Drawing.Point(13, 554);
+            this.btMiamiFilter.Name = "btMiamiFilter";
+            this.btMiamiFilter.Size = new System.Drawing.Size(120, 23);
+            this.btMiamiFilter.TabIndex = 2;
+            this.btMiamiFilter.Text = "Miami Filter";
+            this.btMiamiFilter.UseVisualStyleBackColor = true;
+            this.btMiamiFilter.Click += new System.EventHandler(this.btMiamiFilter_Click);
+            // 
+            // pbNightFilter
+            // 
+            this.pbNightFilter.Location = new System.Drawing.Point(157, 465);
+            this.pbNightFilter.Name = "pbNightFilter";
+            this.pbNightFilter.Size = new System.Drawing.Size(127, 82);
+            this.pbNightFilter.TabIndex = 3;
+            this.pbNightFilter.TabStop = false;
+            // 
+            // btNightFilter
+            // 
+            this.btNightFilter.Location = new System.Drawing.Point(157, 554);
+            this.btNightFilter.Name = "btNightFilter";
+            this.btNightFilter.Size = new System.Drawing.Size(127, 23);
+            this.btNightFilter.TabIndex = 4;
+            this.btNightFilter.Text = "Night Filter";
+            this.btNightFilter.UseVisualStyleBackColor = true;
+            this.btNightFilter.Click += new System.EventHandler(this.btNightFilter_Click);
+            // 
+            // pbHellFilter
+            // 
+            this.pbHellFilter.Location = new System.Drawing.Point(308, 465);
+            this.pbHellFilter.Name = "pbHellFilter";
+            this.pbHellFilter.Size = new System.Drawing.Size(127, 82);
+            this.pbHellFilter.TabIndex = 5;
+            this.pbHellFilter.TabStop = false;
+            // 
+            // btHellFilter
+            // 
+            this.btHellFilter.Location = new System.Drawing.Point(308, 554);
+            this.btHellFilter.Name = "btHellFilter";
+            this.btHellFilter.Size = new System.Drawing.Size(127, 23);
+            this.btHellFilter.TabIndex = 6;
+            this.btHellFilter.Text = "Hell Filter";
+            this.btHellFilter.UseVisualStyleBackColor = true;
+            this.btHellFilter.Click += new System.EventHandler(this.btHellFilter_Click);
+            // 
+            // btLoad
+            // 
+            this.btLoad.Location = new System.Drawing.Point(13, 424);
+            this.btLoad.Name = "btLoad";
+            this.btLoad.Size = new System.Drawing.Size(75, 23);
+            this.btLoad.TabIndex = 7;
+            this.btLoad.Text = "Load image";
+            this.btLoad.UseVisualStyleBackColor = true;
+            this.btLoad.Click += new System.EventHandler(this.btLoad_Click);
+            // 
+            // pbZenFilter
+            // 
+            this.pbZenFilter.Location = new System.Drawing.Point(460, 465);
+            this.pbZenFilter.Name = "pbZenFilter";
+            this.pbZenFilter.Size = new System.Drawing.Size(127, 82);
+            this.pbZenFilter.TabIndex = 8;
+            this.pbZenFilter.TabStop = false;
+            // 
+            // pbBlackAndWhite
+            // 
+            this.pbBlackAndWhite.Location = new System.Drawing.Point(611, 465);
+            this.pbBlackAndWhite.Name = "pbBlackAndWhite";
+            this.pbBlackAndWhite.Size = new System.Drawing.Size(127, 82);
+            this.pbBlackAndWhite.TabIndex = 9;
+            this.pbBlackAndWhite.TabStop = false;
+            // 
+            // pbSwapFilter
+            // 
+            this.pbSwapFilter.Location = new System.Drawing.Point(761, 465);
+            this.pbSwapFilter.Name = "pbSwapFilter";
+            this.pbSwapFilter.Size = new System.Drawing.Size(127, 82);
+            this.pbSwapFilter.TabIndex = 10;
+            this.pbSwapFilter.TabStop = false;
+            // 
+            // btZenFilter
+            // 
+            this.btZenFilter.Location = new System.Drawing.Point(460, 554);
+            this.btZenFilter.Name = "btZenFilter";
+            this.btZenFilter.Size = new System.Drawing.Size(127, 23);
+            this.btZenFilter.TabIndex = 11;
+            this.btZenFilter.Text = "Zen Filter";
+            this.btZenFilter.UseVisualStyleBackColor = true;
+            this.btZenFilter.Click += new System.EventHandler(this.btZenFilter_Click);
+            // 
+            // btBlackAndWhite
+            // 
+            this.btBlackAndWhite.Location = new System.Drawing.Point(611, 554);
+            this.btBlackAndWhite.Name = "btBlackAndWhite";
+            this.btBlackAndWhite.Size = new System.Drawing.Size(127, 23);
+            this.btBlackAndWhite.TabIndex = 12;
+            this.btBlackAndWhite.Text = "Black and White";
+            this.btBlackAndWhite.UseVisualStyleBackColor = true;
+            this.btBlackAndWhite.Click += new System.EventHandler(this.btBlackAndWhite_Click);
+            // 
+            // btSwapFilter
+            // 
+            this.btSwapFilter.Location = new System.Drawing.Point(761, 554);
+            this.btSwapFilter.Name = "btSwapFilter";
+            this.btSwapFilter.Size = new System.Drawing.Size(127, 23);
+            this.btSwapFilter.TabIndex = 13;
+            this.btSwapFilter.Text = "Swap Filter";
+            this.btSwapFilter.UseVisualStyleBackColor = true;
+            this.btSwapFilter.Click += new System.EventHandler(this.btSwapFilter_Click);
+            // 
+            // lbImages
+            // 
+            this.lbImages.FormattingEnabled = true;
+            this.lbImages.Location = new System.Drawing.Point(679, 13);
+            this.lbImages.Name = "lbImages";
+            this.lbImages.Size = new System.Drawing.Size(192, 394);
+            this.lbImages.TabIndex = 14;
+            // 
+            // btSave
+            // 
+            this.btSave.Location = new System.Drawing.Point(109, 424);
+            this.btSave.Name = "btSave";
+            this.btSave.Size = new System.Drawing.Size(75, 23);
+            this.btSave.TabIndex = 15;
+            this.btSave.Text = "Save";
+            this.btSave.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1107, 612);
+            this.Controls.Add(this.btSave);
+            this.Controls.Add(this.lbImages);
+            this.Controls.Add(this.btSwapFilter);
+            this.Controls.Add(this.btBlackAndWhite);
+            this.Controls.Add(this.btZenFilter);
+            this.Controls.Add(this.pbSwapFilter);
+            this.Controls.Add(this.pbBlackAndWhite);
+            this.Controls.Add(this.pbZenFilter);
+            this.Controls.Add(this.btLoad);
+            this.Controls.Add(this.btHellFilter);
+            this.Controls.Add(this.pbHellFilter);
+            this.Controls.Add(this.btNightFilter);
+            this.Controls.Add(this.pbNightFilter);
+            this.Controls.Add(this.btMiamiFilter);
+            this.Controls.Add(this.pbMiamiFilter);
+            this.Controls.Add(this.pbMainPicture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbMainPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMiamiFilter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNightFilter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHellFilter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbZenFilter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBlackAndWhite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSwapFilter)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbMainPicture;
+        private System.Windows.Forms.PictureBox pbMiamiFilter;
+        private System.Windows.Forms.Button btMiamiFilter;
+        private System.Windows.Forms.PictureBox pbNightFilter;
+        private System.Windows.Forms.Button btNightFilter;
+        private System.Windows.Forms.PictureBox pbHellFilter;
+        private System.Windows.Forms.Button btHellFilter;
+        private System.Windows.Forms.Button btLoad;
+        private System.Windows.Forms.PictureBox pbZenFilter;
+        private System.Windows.Forms.PictureBox pbBlackAndWhite;
+        private System.Windows.Forms.PictureBox pbSwapFilter;
+        private System.Windows.Forms.Button btZenFilter;
+        private System.Windows.Forms.Button btBlackAndWhite;
+        private System.Windows.Forms.Button btSwapFilter;
+        private System.Windows.Forms.ListBox lbImages;
+        private System.Windows.Forms.Button btSave;
+    }
+}
+
---
+++ GurshchenkovaValette/GurshchenkovaValette/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GurshchenkovaValette
+{
+    public partial class Form1 : Form
+    {
+        System.Drawing.Image Origin;
+        Bitmap map;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btLoad_Click(object sender, EventArgs e)
+        {
+            GetAPath();
+        }
+        public void GetAPath()
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            DialogResult dr = op.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                string path = op.FileName;
+
+                LoadImage(path, pbMainPicture);
+                LoadImage(path, pbMiamiFilter);
+                LoadImage(path, pbNightFilter);
+                LoadImage(path, pbHellFilter);
+                LoadImage(path, pbZenFilter);
+                LoadImage(path, pbBlackAndWhite);
+                LoadImage(path, pbSwapFilter);
+
+                map = new Bitmap(pbMainPicture.Image);
+                Origin = pbMainPicture.Image;
+
+                
+            }
+        }
+        public void LoadImage(string path, PictureBox pictureBox) 
+        {
+            pictureBox.Load(path);
+            Bitmap temp = new Bitmap(pictureBox.Image,
+               new Size(pictureBox.Width, pictureBox.Height));
+            pictureBox.Image = temp;
+            if (pictureBox.Name == "pbMiamiFilter") pictureBox.Image = ImageFilters.ApplyFilter(new Bitmap(pictureBox.Image), 1, 1, 10, 1);
+            if (pictureBox.Name == "pbNightFilter") pictureBox.Image = ImageFilters.ApplyFilter(new Bitmap(pictureBox.Image), 1, 1, 1, 25);
+            if (pictureBox.Name == "pbHellFilter") pictureBox.Image = ImageFilters.ApplyFilter(new Bitmap(pictureBox.Image), 1, 1, 10, 15);
+            if (pictureBox.Name == "pbZenFilter") pictureBox.Image = ImageFilters.ApplyFilter(new Bitmap(pictureBox.Image), 1, 10, 1, 1);
+            if (pictureBox.Name == "pbBlackAndWhite") pictureBox.Image = ImageFilters.BlackWhite(new Bitmap(pictureBox.Image));
+            if (pictureBox.Name == "pbSwapFilter") pictureBox.Image = ImageFilters.ApplyFilterSwap(new Bitmap(pictureBox.Image));
+
+        }
+
+        private void btMiamiFilter_Click(object sender, EventArgs e)
+        {
+            ApplyMiamiFilter();
+        }
+        private void pbMiamiFilter_Click(object sender, EventArgs e)
+        {
+            ApplyMiamiFilter();
+        }
+
+        private void ApplyMiamiFilter() {
+            pbMainPicture.Image = Origin;
+            pbMainPicture.Image = ImageFilters.ApplyFilter(new Bitmap(pbMainPicture.Image), 1, 1, 10, 1);
+        }
+
+        private void btNightFilter_Click(object sender, EventArgs e)
+        {
+            pbMainPicture.Image = Origin;
+            pbMainPicture.Image = ImageFilters.ApplyFilter(new Bitmap(pbMainPicture.Image), 1, 1, 1, 25);
+        }
+
+        private void btHellFilter_Click(object sender, EventArgs e)
+        {
+            pbMainPicture.Image = ImageFilters.ApplyFilter(new Bitmap(pbMainPicture.Image), 1, 1, 10, 15);
+        }
+
+        private void btZenFilter_Click(object sender, EventArgs e)
+        {
+            pbMainPicture.Image = Origin;
+            pbMainPicture.Image = ImageFilters.ApplyFilter(new Bitmap(pbMainPicture.Image), 1, 10, 1, 1);
+        }
+
+        private void btBlackAndWhite_Click(object sender, EventArgs e)
+        {
+            pbMainPicture.Image = Origin;
+            pbMainPicture.Image = ImageFilters.BlackWhite(new Bitmap(pbMainPicture.Image));
+        }
+
+        private void btSwapFilter_Click(object sender, EventArgs e)
+        {
+            pbMainPicture.Image = Origin;
+            pbMainPicture.Image = ImageFilters.ApplyFilterSwap(new Bitmap(pbMainPicture.Image));
+        }
+
+        
+    }
+}
